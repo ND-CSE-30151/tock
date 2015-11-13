28aa8d5159988cc0a054aee606651507e9f06a03
David Chiang <dchiang@nd.edu>
2015-11-13 15:24:18 -0500

    setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='tock',
+      version='0.1',
+      description='Theory of Computing Toolkit',
+      author='David Chiang',
+      author_email='dchiang@nd.edu',
+      url='https://github.org/davidweichiang/tock',
+      license='MIT',
+      packages=['tock'],
+      install_requires=['six'],
+      )

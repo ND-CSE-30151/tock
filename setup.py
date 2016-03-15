121639718442606282c04645be9043789c7aba28
davidweichiang <dchiang@nd.edu>
2016-03-15 10:11:08 -0400

    version 0.13

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tock',
-      version='0.12',
+      version='0.13',
       description='Theory of Computing Toolkit',
       author='David Chiang',
       author_email='dchiang@nd.edu',

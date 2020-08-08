eace1a04c00ca8f58f614b97719f3e6e635a228a
David Chiang <dchiang@nd.edu>
2020-08-08 15:42:23 -0400

    improve tree layout

--- tock/trees.py
+++ tock/trees.py
@@ -6,21 +6,45 @@ class Tree:
         self.children = children or []
 
     def svg(self):
-        node_width = 32
+        node_width = 10 # per character, approximate
         node_height = 16
-        sister_distance = 64
-        level_distance = 32
-        raise_label = 3
+        node_depth = 4
+        sister_distance = 16
+        level_distance = 16
 
-        def height(node):
-            if len(node.children) == 0:
-                return 0
+        pos = {}
+        def layout(node, x1, y1):
+            """Layout the tree rooted at node, with upper-left corner at x1, y1.
+            
+            Returns: the lower-right corner.
+            """
+            if len(node.children) > 1:
+                x2, y2 = x1, y1
+                cy1 = y1 + node_height+node_depth+level_distance
+                for ci, child in enumerate(node.children):
+                    if ci > 0:
+                        x2 += sister_distance
+                    x2, cy2 = layout(child, x2, cy1)
+                    y2 = max(y2, cy2)
+                nx = (pos[node.children[0]][0] + pos[node.children[-1]][0])/2
+            elif len(node.children) == 1:
+                child = node.children[0]
+                cx1 = x1
+                cy1 = y1 + node_height+node_depth+level_distance
+                if len(node.label) > len(child.label):
+                    cx1 += (len(node.label) - len(child.label)) * node_width / 2
+                x2, y2 = layout(child, cx1, cy1)
+                nx = pos[child][0]
             else:
-                return 1+max(height(child) for child in node.children)
-        h = height(self)
+                nx = x1 + node_width*len(node.label)/2
+                x2 = x1 + node_width*len(node.label)
+                y2 = y1 + node_height+node_depth
+            pos[node] = (nx, y1+node_height)
+            return x2, y2
+        x2, y2 = layout(self, 0, 0)
                 
         ret = []
-        ret.append(f'<svg height="{h*level_distance+node_height}">\n'
+        ret.append(f'<svg width="{x2}" height="{y2}">\n'
                    '  <style>\n'
                    '    .nonterminal {\n'
                    f'      font: {node_height}px serif;\n'
@@ -31,25 +55,18 @@ class Tree:
                    '      text-anchor: middle;\n'
                    '    }\n'
                    '  </style>\n')
-        def draw(node, x, y):
+        
+        for node in pos:
             cls = 'nonterminal' if len(node.children) > 0 else 'terminal'
             label = xml.sax.saxutils.escape(node.label)
+            nx, ny = pos[node]
+            ret.append(f'<text x="{nx}" y="{ny}" class="{cls}">{label}</text>\n')
             if len(node.children) > 0:
-                cxs = []
                 for child in node.children:
-                    cx, x = draw(child, x, y+level_distance)
-                    cxs.append(cx)
-                nx = (cxs[0] + cxs[-1])/2
-                for cx in cxs:
-                    ret.append(f'<line x1="{nx}" y1="{y}" x2="{cx}" y2="{y+level_distance-node_height}" style="stroke:rgb(0,0,0);stroke-width:2"/>\n')
-            else:
-                nx = x
-                x += sister_distance
-            ret.append(f'<text x="{nx}" y="{y-raise_label}" class="{cls}">{label}</text>\n')
-            return nx, x
-        draw(self, node_width/2, node_height)
+                    cx, cy = pos[child]
+                    ret.append(f'<line x1="{nx}" y1="{ny+node_depth}" x2="{cx}" y2="{cy-node_height}" style="stroke:rgb(0,0,0);stroke-width:1"/>\n')
         ret.append('</svg>\n')
-        
+
         return ''.join(ret)
 
     def _repr_svg_(self):

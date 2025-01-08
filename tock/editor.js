63bfc38ec31b5fa3d38dfd9b2ccc93fd142d3476
David Chiang <dchiang@nd.edu>
2025-01-08 16:05:51 -0500

    mouse cursor

--- tock/editor.js
+++ tock/editor.js
@@ -928,7 +928,8 @@ function main(ei) {
     canvas = document.createElement("canvas");
     canvas.setAttribute("style", "outline: 1px solid gray; background: white; margin: 1px; width: 600px; height: 600px;");
     canvas.setAttribute("tabindex", -1); // make canvas focusable
-    canvas.setAttribute("contenteditable", true);
+    canvas.setAttribute("contenteditable", true); // make keyboard appear on tablets
+    canvas.style.cursor = "default"; // don't change cursor to 'text' on desktop
     canvas_dpr = window.devicePixelRatio || 1;
     canvas.width = 600 * canvas_dpr;
     canvas.height = 600 * canvas_dpr;
